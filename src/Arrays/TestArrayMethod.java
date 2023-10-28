523b90249f1dac5a2f9ffff6d5d08ff658ab20d9
RomualdoFranca <romualdo.ads2022@gmail.com>
2023-10-28 17:38:34 -0300

    arrays methods

--- src/Arrays/TestArrayMethod.java
+++ src/Arrays/TestArrayMethod.java
@@ -1,4 +1,36 @@
 package Arrays;
 
 public class TestArrayMethod {
+    //Java Program to demonstrate the way of passing an array to method.
+    public static void main(String[] args) {
+
+        int b[] = {10, 20, 30, 40};
+        int c[] = {5, 4, 3, 2, 1};
+
+        min(b);
+        printArray(c);
+
+    }
+
+    //creating a method which receives an array as a parameter
+    static void min(int arr[]) {
+
+        int min = arr[0];
+
+        for (int i = 0; i < arr.length; i++) { // logica para percorrer todos os elementos da array na sequencia
+
+            if (min < arr[i]) {
+                min = arr[i];
+            }
+            System.out.printf("Indice %d = %d: \n", i, min);
+//            System.out.println(min);
+        }
+    }
+
+    static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.printf("%d ", arr[i]);
+        }
+
+    }
 }
